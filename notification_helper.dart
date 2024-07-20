cd6726760b1472bb6b16163c1b0b94baff342827
eisa ibrahim <68941204+eisaibrahem@users.noreply.github.com>
2024-07-20 12:32:19 +0300

    Update notification_helper.dart

--- notification_helper.dart
+++ notification_helper.dart
@@ -41,20 +41,19 @@ class NotificationsHelper {
 
   Future<String?> getAccessToken() async {
     final serviceAccountJson = {
-      "type": "service_account",
-      "project_id": "missing-egypt-a2a74",
-      "private_key_id": "62f52c298a3017e941df8cce45cf81d2c0c90d52",
-      "private_key":
-          "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCS4H3UjsVV9QOr\nmGPCNWwS7WZS+4mhb/sZP0Kq41ia7+7RnQawgA1OnK4rU+DKzjOa/WASQDIbSIZ3\nVrZoMPPCKw5xdYP+LPeJDzVk8GazcFaFAo2rUGlWtZVKTad7ncI8mMGWE/G1Szqa\nE8xvlTgT8tFqYENUWbCl7MozUdOVYUEDO/NiHeb7vIUWq5MFH2ttGGxkhc+Ec/FL\nSB2HM5p5iq9AJXHQrnMhCuG9EKU1KAMxfKHifRPcfrTRxRS++x+LPTUt2KPTcdAo\nxSIWx7aTqPGbXnEp10A8A2AyzFsPzy2XlLObKR0/rk4lODxUBrFIzGEozNW8ynqB\nSI00JS1hAgMBAAECggEABHws83zxmzSaalbCsxdBB9nufjm2o/Kgsw5quX+sQLzz\naLy4C9M5NM2VhvauflrqGgFDOgNyVEF0c7f25XsbMnJwpDfPskcKvwlzGrQ5mqCj\nT7f6fgD8Wny8hJKW+vAgEowimzPcNWpI7ZQNsdmXZwqK6Qnr/GspuQNgUE1fHehY\nxUu1svZ2bIoJQn/gMYwu+pRgX6C8Wt6M53eIW8IkROkfyfrRJ9YbgRG3AAqNCAzm\nXuulYHPr9uVOl8PXs1kcW6+edEnZPwXQi0sk8aB9HuHLbNcAR1FZempBWb8XTyLN\n9CvIq8Iolcnwxl7VQhq1cjEVg8TvvtCT7xGkz7Y+QQKBgQDDqVuJWfK9XTkcnJnm\nNP3rMWqUj+ndZ1RE0DJz8VV047WR+UO41vFeFnjs9q3q6Ww3+ft9gdFOdBjXy8Lu\nZrBhwc9D3UIEIiSEQ1K1UltnA8Q+KNHrAoU+weElS33A/adDI9o6VlJw+g4QV/Wg\nBN+wDm+ZWp2Qoux8l7d3vXbvkQKBgQDAK8vBeaUd6g4hkpSTlzCwIzNt+eJFodIm\ntMjeaUFw9JpXb2s4I/NXN6JKADsgjhGjZIe5U+/Nnjevieb4nv3uU5YHErNGAU7j\nDU1MNXsmWzut5dB3ZumFbLx3urPPvH+4sb0+hCYZbS6TiEwn+VJ9vSglnx3GZWUC\n+nlm5zYY0QKBgEN17P2aabsonTxkMhvRQYsJ6rl2NSgFLGTdc8fN+aznpf7CdrsQ\nQQAzt1XcZ3KLMKjMJA3N6KCoHriopHkWJtJNxGydqNlL/FMt8yJZVvJZthvIbzgc\nElzk8/+r25vi2PypK+DZSmtxi8/Ow/18MLO/BnNVbuYjhm4e0T8TsCJBAoGAaDWX\nuaRQoOcyoIwPW/XJ5kpAcmvmgyIchvbUUp+7aBiMtctq1jfQlsRJkl2Lsry33eX/\nH8XfrZ4VznA2uy3kv9+95LoEU3sUH1YwS5pY3NTuiP+ty4IYMk1j6n1pUsaCcFKa\nMBOTYYhukHyB589xJ1RzN+uQRuznGPVg7ieFk3ECgYEAuv4floI1XfmDWdpcbA4P\n8qiCa6N6LuCqnlwyBfQlb5Qipft+ASlQKXnWsm9eVvAeTUqlHHHV3HzaUllgVWem\nYBz1Tv6oLeOYJhEghi86cXc3ztk/lQmPqwVupWs3BJkKm67k11AbWTTSrkL9NBd+\n6+2LwIJu7AOVCk7IrWvYlUY=\n-----END PRIVATE KEY-----\n",
-      "client_email": "missing-eg@missing-egypt-a2a74.iam.gserviceaccount.com",
-      "client_id": "102242598778623857726",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
+      "type": "",
+      "project_id": "",
+      "private_key_id": "",
+      "private_key":"",
+      "client_email": "",
+      "client_id": "",
+      "auth_uri": "",
+      "token_uri": "",
       "auth_provider_x509_cert_url":
-          "https://www.googleapis.com/oauth2/v1/certs",
+          "",
       "client_x509_cert_url":
-          "https://www.googleapis.com/robot/v1/metadata/x509/missing-eg%40missing-egypt-a2a74.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
+          "",
+      "universe_domain": ""
     };
 
     List<String> scopes = [
@@ -123,8 +122,10 @@ class NotificationsHelper {
   }) async {
     try {
       var serverKeyAuthorization = await getAccessToken();
+      
+      // change your project id
       const String urlEndPoint =
-          "https://fcm.googleapis.com/v1/projects/missing-egypt-a2a74/messages:send";
+          "https://fcm.googleapis.com/v1/projects/(YourProjectId)/messages:send";
 
       Dio dio = Dio();
       dio.options.headers['Content-Type'] = 'application/json';
